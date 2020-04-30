dce04fa4e9c64132512f5effa0e56ec87d6acb71
serviolee <serviolee@gmail.com>
2020-04-30 15:27:16 -0700

    created server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port  = 3000;
+
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
