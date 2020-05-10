544b87981833117cf5259e8a4d1b526e5ef7696a
serviolee <serviolee@gmail.com>
2020-05-10 10:43:08 -0700

    displaying pictures in slider correctly

--- client/src/components/ModalSlider.jsx
+++ client/src/components/ModalSlider.jsx
@@ -4,12 +4,15 @@ import ModalSliderImage from './ModalSliderImage.jsx';
 const ModalSlider = React.forwardRef((props, ref) => {
   return (
     <div className="modal-slider-container">
-      <div className="restaurant-name">Photos for Taco Boys</div>
-        <div className="slider-image-wrapper" ref={ref}>
-          {props.photos.map((photo, index) => {
-            return <ModalSliderImage key={index} id={index} photo={photo} photoId={props.photoId} handleSliderClick={props.handleSliderClick} />
-          })}
-        </div>
+      <div className="restaurant-name">
+        <h3>Photos for Taco Boys
+        </h3>
+      </div>
+      <div className="slider-image-wrapper" ref={ref}>
+        {props.photos.map((photo, index) => {
+          return <ModalSliderImage key={index} id={index} photo={photo} photoId={props.photoId} handleSliderClick={props.handleSliderClick} />
+        })}
+      </div>
     </div>
   )
 })
--- client/src/components/ModalSliderImage.jsx
+++ client/src/components/ModalSliderImage.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 const ModalSliderImage = (props) => {
-  // if photoId = props.id, then add class highlight
-  console.log(props);
   return (
       <img className={`modal-slider-image ${props.photoId === props.id ? 'highlight' : ''}`} src={props.photo} id={props.id} onClick={props.handleSliderClick}></img>
   )
