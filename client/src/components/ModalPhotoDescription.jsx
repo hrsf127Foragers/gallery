c7209fb4ec163540df2da8160279ed7664892725
serviolee <serviolee@gmail.com>
2020-05-10 19:22:15 -0700

    added photo description component and styling

--- client/src/components/ModalPhotoDescription.jsx
+++ client/src/components/ModalPhotoDescription.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import Reviewer from './Reviewer.jsx';
-import ShareButton from './ShareButton.jsx';
-import ReportButton from './ReportButton.jsx';
-import HelpfulButton from './HelpfulButton.jsx';
-import NotHelpfulButton from './NotHelpfulButton.jsx';
 
 const ModalPhotoDescription = (props) => {
   return (
    <div className="photo-description-wrapper">
-     <span className="photo-caption">taco salad</span><br/>
-     <span className="photo-date">date</span>
+     <span className="photo-caption">Actual Food</span><br/>
+     <span className="photo-date">January 14, 2020</span>
+     <hr className="horizontal-line"/>
      <Reviewer />
-     <ShareButton />
-     <ReportButton />
-     <HelpfulButton />
-     <NotHelpfulButton />
    </div>
   )
 }
